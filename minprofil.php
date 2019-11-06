9b6bd38b1a95b85cf333c6d5fbf01f7a4b5c2280
Maja <xiastald@hotmail.com>
2019-11-06 13:35:29 +0100

    Update minprofil.php

--- minprofil.php
+++ minprofil.php
@@ -1,1 +1,11 @@
+<!-- her henter jeg headeren ned -->
+<?php
+    $page = ('FAQ');
+    //require_once('includes/header.php');
+    //require_once('includes/footer.php');
+?>
 
+<xh1>MIN PROFIL</xh1>
+
+
+</html>
