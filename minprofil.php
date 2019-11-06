bf914c9b4a8d4472bea7ef278c469db27fcce696
Maja <xiastald@hotmail.com>
2019-11-06 15:49:02 +0100

    Update minprofil.php

--- minprofil.php
+++ minprofil.php
@@ -1,26 +1,33 @@
 <!-- her henter jeg headeren ned -->
 <?php
     $page = ('FAQ');
-    //require_once('includes/header.php');
+    require_once('includes/header.php');
     //require_once('includes/footer.php');
 ?>
 
-<xh1>MIN PROFIL</xh1> <img id="profilbillede" src="images/mutuumudenbaggrund.png">
+<h1>MIN PROFIL</h1> 
+<br>
 
 <?php
-$udyld = "SELECT email, first_name, last_name, adress, city FROM users";
+$udfyld = "SELECT mail, fornavn, efternavn, mobil FROM users";
 
 $result = mysqli_query($con, $udfyld);
-if (!$result) die(mysqli_error($con, $udfyld));
+if (!$result) die(mysqli_error($con));
 
 $row = mysqli_num_rows($result);
 
 if ($row > 0) {
-    while($row = mysqli_fetch_assoc($result));
-        echo "brugernavn: " . $row['eamil'] "<br>" . "Fornavn: " . $row['first_name'] . "<br>" . "Efternavn: " . $row['last_name'] . "<br>" . "By: " . $row['city'] . "<br>" . "Adresse: " . $row['address'] . "<br>";
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "brugernavn: " . $row['mail'] . "Fornavn: " . $row['fornavn'] .  "Efternavn: " . $row['efternavn'] . "Telefon nr.: " . $row['mobil'] . "<br>";
+    
+        } 
+    } else {
+    echo "<br>Data er ikke blevet oplyst";
 }
 
+mysqli_close($con);
     
 ?>
+
 </body>
 </html>
