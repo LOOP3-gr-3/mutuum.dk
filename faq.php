e22188a1c39d1798b29f321b3e7c59e1f6fbe4b4
Maja <xiastald@hotmail.com>
2019-11-06 11:48:58 +0100

    Update faq.php

--- faq.php
+++ faq.php
@@ -4,11 +4,18 @@
     require_once('includes/header.php');
 ?>
 
-<link href="styles/om_os.css" type="text/css" rel="stylesheet">
+<link href="styles/overallstyles" type="text/css" rel="stylesheet">
 
 
 <!-- Dette er overskriften for dette indhold -->
-<h1><strong>FAQ</strong></h1>
-<p>Dette </p>
-<U>Hvordan sender jeg en kontrakt direkte til en i mit netværk?</U>
-<svar></svar>
+<xh1><strong>FAQ</strong></xh1>
+
+<p>Denne sider indeholder de mest stillede spørgsmål. Hvis der ikke er svar på det du søger, så endelig benyt kontaktinformationerne i bunden</p>
+
+<!-- med U får jeg html til at lave understregning på den teskt indenfor den kommando-->
+<U>Hvordan tilføjer jeg en til mit netværk?</U>
+<p>Du skal gå ind i mit netværk fra min side, søge den bestemte bruger ud via mail, i søgefeltet. Når denne dukker op klipper du in på brugeren og der vil være et ikon, hvor kan man tilføje personen til sit netværk ved at klippe på hjertet og "profilbilledet"</p> <br>
+
+<U>Kan jeg lave en kontrakt uden gebyrer med mine nære relationer?<U>
+<p></p>
+
