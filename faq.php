fa55e8bb06ef730d5810531720ca21f0f31eec4b
Maja <xiastald@hotmail.com>
2019-11-06 11:58:03 +0100

    Update faq.php

--- faq.php
+++ faq.php
@@ -2,6 +2,7 @@
 <?php
     $page = ('FAQ');
     require_once('includes/header.php');
+    //require_once('includes/footer.php');
 ?>
 
 <link href="styles/overallstyles" type="text/css" rel="stylesheet">
@@ -14,8 +15,10 @@
 
 <!-- med U får jeg html til at lave understregning på den teskt indenfor den kommando-->
 <U>Hvordan tilføjer jeg en til mit netværk?</U>
-<p>Du skal gå ind i mit netværk fra min side, søge den bestemte bruger ud via mail, i søgefeltet. Når denne dukker op klipper du in på brugeren og der vil være et ikon, hvor kan man tilføje personen til sit netværk ved at klippe på hjertet og "profilbilledet"</p> <br>
+<p>Du skal gå ind i mit netværk fra min side, søge den bestemte bruger ud via mail, i søgefeltet. Når denne dukker op klipper du in på brugeren og der vil være et ikon, hvor kan man tilføje personen til sit netværk ved at klippe på hjertet og "profilbilledet".</p> <br>
 
-<U>Kan jeg lave en kontrakt uden gebyrer med mine nære relationer?<U>
-<p></p>
+<U>Kan jeg lave en kontrakt uden gebyrer med mine nære relationer?</U>
+<p>Der vil altid være et adminstrationsgebyr for at oprette og eksekvere kontrakten, men der kan vælges en 0% rente, hvis dette ønskes.</p> <br>
 
+<U>Investering af større puljer?</U>
+<p>Hvis der ønskes at få afkast på et større beløb, så kan der oprettes en ønsket kontrakt på 2-12.000 kr. kontrakten kan herefter replikkeres, ved at vælge eks. 10 ved feltet antal. Ønskes der flere forskellige kontrakter tilføjer man blot på inkonet + oppe i højre hjørne.</p>
