950c2c607a416a5f289ab4cec071e8130176bf67
Maja <xiastald@hotmail.com>
2019-11-07 14:11:49 +0100

    Update minprofil.php

--- minprofil.php
+++ minprofil.php
@@ -41,13 +41,11 @@ mysqli_close($con);
 
 <!-- U sikre at der kunne en understreg under skriften-->
 <U>Rating</U>
-<p>Denne sker gennem kreditverificingen og der dermed udenfor afgrænsningen. Men I dette felt ville der skulle trækkes data fra kreditverificingen, denne data skal kunne aflæse om hvorvidt brugeren for en AAA, AA, A, B eller C rating</p><br>
+<p>Denne sker gennem kreditvurderingen og der dermed udenfor afgrænsningen. Men I dette felt ville der skulle trækkes data fra kreditverificingen, denne data skal kunne aflæse om hvorvidt brugeren for en AAA, AA, A, B eller C rating</p><br>
 <p>Din rating er blevet givet på baggrund af din kreditvurdering. Denne kan anmodes om at blive fornyet under "ret oplysninger"</p>
 
 <!-- denne kode sikre, at brugeren bliver ført over til en nye side, når der klikkes på ret oplysninger-->
 <a href="retoplysninger.php" target="_blank"><h3>Ret oplysninger</h3></a>
-
-
 
 
 </body>
