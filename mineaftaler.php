1bb055e2d2e0694545ebeb1d1da82c265f849175
Maja <xiastald@hotmail.com>
2019-11-07 15:56:57 +0100

    permission

--- mineaftaler.php
+++ mineaftaler.php
@@ -7,3 +7,26 @@
         header('location:forside.php');
 }
 ?>
+<h1>Mine aftaler</h1>
+<p>Her ses alle dine aftaler, de er sorteret efter dato, der kan søges efter en bestemt mail, hvis man søger et bestemt lån </p>
+
+<?php
+$udfyld = "SELECT mail, fornavn, efternavn, mobil FROM users";
+
+$result = mysqli_query($con, $udfyld);
+if (!$result) die(mysqli_error($con));
+
+$row = mysqli_num_rows($result);
+
+if ($row > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "brugernavn: " . $row['mail'] . "Fornavn: " . $row['fornavn'] .  "Efternavn: " . $row['efternavn'] . "Telefon nr.: " . $row['mobil'] . "<br>";
+    
+        } 
+    } else {
+    echo "<br>Data er ikke blevet oplyst. Opret bruger eller oplys manglende data";
+}
+
+mysqli_close($con);
+    
+?>
