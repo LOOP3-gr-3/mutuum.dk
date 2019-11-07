f6fa8652809adb73ff46eef9cb0f8f1e1b87655c
Mette <metteweyergang@gmail.com>
2019-11-07 15:27:19 +0100

    updateminside

--- minside.php
+++ minside.php
@@ -4,31 +4,29 @@ require_once('includes/header.php');
 ?>
 
 <!-- Følgende er lavet vha. Bootstrap, hvor billederne bliver stilllet i Bootstraps grid system -->
+<!-- Ikonerne er fra Word Documents 2016 -->
 <div class="row">
-  <div class="col">
-      <img src="images/profil.png" alt="MinProfil" style="width:90%">
+  <div class="col-mew"> 
+      <a href="minprofil.php"><img src="images/profil.png" alt="MinProfil" style="width:90%"></a>
     </div>
-  <div class="col">
-      <img src="images/kontrakt.png" alt="Opret kontrakt" style="width:90%">
+  <div class="col-mew">
+      <a href="#KONTRAKT"><img src="images/kontrakt.png" alt="Opret kontrakt" style="width:90%" style="center"></a>
     </div>
+    <div class="col-lille"></div>
 </div>
 <div class="row">
-<div class="col" class="mtextcenter"><a id="mprofil" href="minprofil.php" target="_blank">Min Profil</a></div>
-</div>
-<div class="row">
-  <div class="col">
-      <img src="images/match.png" alt="Matchsiden" style="width:90%">
+ <div class="col-mew"> 
+      <a href="#MATCH"><img src="images/match.png" alt="Matchsiden" style="width:90%"></a>
     </div>
-  <div class="col">
-      <img src="images/netvaerk.png" alt="Netværk" style="width:90%">
+  <div class="col-mew">
+      <a href="#NETVÆRK"><img src="images/netvaerk.png" alt="Netværk" style="width:90%"></a>
     </div>
 </div> 
 <div class="row">
-  <div class="col">
-      <img src="images/graf.png" alt="Overblik" style="90%">
+ <div class="col-mew"> 
+      <a href="#OVERBLIK"><img src="images/graf.png" alt="Matchsiden" style="width:90%"></a>
     </div>
-</div>
-      
+
 
 </body>
 </html>
