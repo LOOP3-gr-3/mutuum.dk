6540b1f813e389bbaa2ff178383156dfc55b9cf8
Maja <xiastald@hotmail.com>
2019-11-07 13:22:42 +0100

    Update minprofil.php

--- minprofil.php
+++ minprofil.php
@@ -1,6 +1,6 @@
 <!-- her henter jeg headeren ned -->
 <?php
-    $page = ('FAQ');
+    $page = ('Min profil');
     require_once('includes/header.php');
     //require_once('includes/footer.php');
 ?>
@@ -22,12 +22,29 @@ if ($row > 0) {
     
         } 
     } else {
-    echo "<br>Data er ikke blevet oplyst";
+    echo "<br>Data er ikke blevet oplyst. Opret bruger eller oplys manglende data";
 }
 
 mysqli_close($con);
     
 ?>
+
+<!-- denne kode sikre, at brugeren bliver ført over til en nye side, når der klikkes på mine aftaler-->
+<a href="mineaftaler.php" target="_blank"><br><br><h3>Mine aftaler</h3></a>
+
+<U>Kredit oplysninger</U>
+<p>Disse oplysninger skal hentes over kredit verificeringen, det er derfor udenfor afgrænsningen, da dette er outsourcet</p>
+
+<!-- U sikre at der kunne en understreg under skriften-->
+<U>Rating</U>
+<p>Denne sker gennem kreditverificingen og der dermed udenfor afgrænsningen. Men I dette felt ville der skulle trækkes data fra kreditverificingen, denne data skal kunne aflæse om hvorvidt brugeren for en AAA, AA, , B eller C rating</p><br>
+<p>Din rating er blevet givet på baggrund af din kreditvurdering. Denne kan anmodes om at blive fornyet under "ret oplysninger"</p>
+
+<!-- denne kode sikre, at brugeren bliver ført over til en nye side, når der klikkes på ret oplysninger-->
+<a href="retoplysninger.php" target="_blank"><h3>Ret oplysninger</h3></a>
+
+
+
 
 </body>
 </html>
