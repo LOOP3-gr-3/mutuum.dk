89ad6a2a087df7aa377f513fbad00768ba67b75b
Maja <xiastald@hotmail.com>
2019-11-18 13:02:36 +0100

    Mineaftaler skelet

--- mineaftaler.php
+++ mineaftaler.php
@@ -7,3 +7,31 @@
         header('location:forside.php');
 }
 ?>
+
+<h2>MINE AFTALER</h2>
+
+<!-- INDSÆT SØGEFELT-->
+
+<I>Din oversigt over alle aftaler</I><br><br>
+
+<h3>Historik</h3>
+
+<!--Her samles alt der skal i tabellen i en samlet container med ID´et "aftalehistorik"-->
+<container id="aftalehistorik"> 
+
+<!-- #nbsp er skrevet ind for boxen bliver fixeret til at være den givne størrelse uden at skulle benytte row and colums-->
+<table>
+    <tr>
+        <th id="Xdato">Dato &nbsp;&nbsp;&nbsp;&nbsp; </th>
+        <th id="Xmail">Mail &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; </th>
+        <th id="Xbeløb">Beløb &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; </th>
+    </tr>
+    <!-- nedenstående er felterne der udfyldes når man indgår en aftale, så den bliver en del af historikken. Se forhåndsvisning for at se hvordan det hænger sammen. td = indhold til kolonner, th overskrift til kolonner og tr angiver ny "linje" -->
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
+    
+</container>
