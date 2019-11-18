83186f2673e919ea9aff1449a1baca3fe7f86a85
Maja <xiastald@hotmail.com>
2019-11-18 11:33:40 +0100

    Headings

--- om_os.php
+++ om_os.php
@@ -8,7 +8,7 @@
 
 
 <!-- Dette er overskriften for dette indhold -->
-<h1><strong>HVEM ER VI?</strong></h1>                                
+<h3><strong>HVEM ER VI?</strong></h3>                                
 
 <p>MUTUUM startede på baggrund af et ønske - fra Morten Refsgaard, Jens Eriksen og Sander Dyrvig, om at hjælpe dem, der havde brug for lidt ekstra penge til indskud til diverse efter nødssituationer. Men også ønsket om at gøre det lettere for to bekendte at låne penge til hinanden, men sikre at den økonomiske situation ikke kommer mellem dem vha. af en juridisk kontrakt, der skal holde begge parter ansvarlige for aftalen. <br> Efterfølgende er hele designet, forretningmodellen og det videre koncept udarbejdet af en gruppe 3. semester studerende fra Aarhus Universitet:
 </p>
@@ -21,7 +21,7 @@
         <li>Lasse Hvisthule</li>
     </ul>
 
-<h1><strong>HVORFOR VÆLGE OS?</strong></h1>
+<h3><strong>HVORFOR VÆLGE OS?</strong></h3>
 <ul>
     <li>Enkelte, overskuelige og juridiskbindende kontrakter</li>
     <li>Slip for at sende en reminder eller komme i uønskede situationer med nære relationer</li>
