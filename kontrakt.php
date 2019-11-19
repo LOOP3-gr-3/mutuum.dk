10dcc8ca6ca9ee15f35b80025fe0a498fe7a91f4
Maja <xiastald@hotmail.com>
2019-11-19 10:04:33 +0100

    Oprettet css til footer igen

--- kontrakt.php
+++ kontrakt.php
@@ -2,10 +2,15 @@
 <?php
     $page = ('Mine aftaler');
     require_once('includes/header.php');
-    //require_once('includes/footer.php');
+    require_once('includes/footer.php');
     if (isset($_SESSION['user_id'])) {
         header('location:forside.php');
 }
 ?>
 
 <h1 id="xopretudlan">OPRET UDLÅN</h1>
+
+<container id="xbutton">
+    <button>Låneaftale via netværk</button><button>Låneaftale via Matchsiden</button>
+</container>
+
