cac51be015b3c1db2a69e1baf7c73d06a05680ec
Maja <xiastald@hotmail.com>
2019-11-19 09:20:06 +0100

    Inkluderet header

--- forside.php
+++ forside.php
@@ -1,4 +1,14 @@
-<link href=includes/styles/forside.css type="text/css" rel="stylesheet">
+<!-- her henter jeg headeren ned -->
+<?php
+    $page = ('Mine aftaler');
+    require_once('includes/header.php');
+    //require_once('includes/footer.php');
+    if (isset($_SESSION['user_id'])) {
+        header('location:forside.php');
+}
+?>
+
+<link href="includes/styles/forside.css" type="text/css" rel="stylesheet">
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
