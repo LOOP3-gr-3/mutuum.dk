cf6b80cfc99bc77c8e223dc1e4bd891a6ede2a13
WasfiaHashemi <57392357+WasfiaHashemi@users.noreply.github.com>
2019-11-19 17:22:01 +0100

    Footer og forside

--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,12 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-.wfooter {
+    
+a{
+    text-decoration: none;
+    outline: none;
+}
+.footer {
    position: fixed;
    left: 0;
    bottom: 0;
@@ -15,11 +20,78 @@
    color: white;
    text-align: center;
 }
+/*De fire næste kolonner angiver størrelsen,  placeringen, afstanden, at de skal være på linje og ikke ovenover hinanden og hvilken form teksten skal stå i*/
+.wfooter-menu-en {
+    width: 100%;
+    height: 4em;
+    margin-top: 1em;
+}
+.wfooter-menu-en ul{ 
+    text-align: center;
+
+}
+.wfooter-menu-en ul li a{
+    font-family: arial;
+    font-size: 2.5em;
+    font-weight: 600;
+    color: #fff;
+    text-transform: uppercase;
+}
+#wkomigang {   
+   text-decoration: none;
+    color:white;
+    font-size:2em;
+    margin:1em 1em 1em;
+    
+}
+#wkomigang{
+    position:relative; 
+    width: 25%;
+    left:45%;   
+    opacity:0.6;
+    margin-top:5%;
+    margin-bottom: 2%;
+    text-align:center;
+    border:0.09em solid black;
+    border-radius:20px;
+    text-decoration: none;
+}
+
+/*De tre næste kolonner angiver størrelsen,  placeringen, afstanden, at de skal være på linje og ikke ovenover hinanden og hvilken form teksten skal stå i*/
+}
+.wfooter-menu-to{
+    width: 100%;
+    height: auto;
+    margin: auto;
+  
+    
+    
+}
+        
+.wfooter-menu-fire p{
+    width:  100%;
+    height: auto;
+    margin: auto;
+   
+}
+.wfooter-menu-fire p{
+    margin: 0em;
+    padding: 0em;
+    text-align: center;
+}
+.wfooter-menu-fire p{
+    font-family: arial;
+    font-size: 1em;
+    font-weight: 600;
+    color: #fff;
+    text-transform: uppercase;
+}
+
 </style>
 </head>
 <body>
 
-<div class="wfooter">
+<div class="footer">
     <div class="wfooter-menu-en">
             
                 <a id="wkomigang" href="">KOM I GANG</a>
