93522387a8b31cdf51df2a311c273466bbd425f5
Lasse <lassehvisthule@outlook.dk>
2019-11-20 08:38:55 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,27 +1,27 @@
 <div class="footer">
     <div class="wfooter-menu-en">
             
-                <a id="wkomigang" href="">KOM I GANG</a>      
+             <p>dfdjfj</p>
     </div>
     
     <br>
     
     <div class="wfooter-menu-to">
-        <div class="col-lg-3 col-md-6 footer-info">
-            
-                <a id="woplysninger" href="../faq">FAQ |</a>
+        
+            <div class="las-container">
+                <a href="../faq">FAQ |</a>
                 <a href="../handelsebetingelser">Handelsbetingelser |</a>
                 <a href="../kontakt">Kontakt</a>
-            
-        </div>
+            </div>
+        
     </div>
     <br>
             <div class="wfooter-menu-tre">
-                <div class="col-lg-3 col-md-6 footer-info">
+                
                     <p>CVR: 23850592 <br>
                     Adresse: Birk Centerpark 15<br>
                     Mail: Mutuum@hotmail.com </p>
-                </div>
+             
             </div>
     
 </div>
