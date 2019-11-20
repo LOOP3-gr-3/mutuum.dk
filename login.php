976f3e5e8adc9a74031a908f1b3bd9bd08a82f2a
Mette <metteweyergang@gmail.com>
2019-11-20 09:38:10 +0100

    kommentar til login

--- login.php
+++ login.php
@@ -3,29 +3,29 @@ $page = 'Log Ind';
 require_once('includes/header.php');
 
 if(isset($_SESSION['user_id'])) {
-    header('Location: minside.php');
+    header('Location: minside.php'); /*Her bliver brugeren dirigeret til minside, hvis de er logget ind */
 }
 if (isset($_COOKIE['login'])) {
-	$mail = $_COOKIE['login'];
+	$mail = $_COOKIE['login']; /* dette gør, at der gemmer sig en cookie på harddisken, som gør, at den husker brugerens brugernavn/mail */
 } else {
 	$mail = '';
 }
-if(isset($_POST['mail']) && isset($_POST['password'])) {
+if(isset($_POST['mail']) && isset($_POST['password'])) { /* Her tjekker vi for at både mail og password er sat */
     $mail_temp = $_POST['mail'];
     $password = $_POST['password'];
     $query = "SELECT * FROM users WHERE mail = '$mail_temp'";
     $result = mysqli_query($con, $query);
-    if (!$result){
+    if (!$result){ /* Hvis der ikke er kontakt til server, skriver den en fejl til brugeren */
         echo "MySQL Error: " . mysqli_error($con);
         require_once ('includes/footer.php');
         die();  
     } 
     else {
-        $rows = mysqli_num_rows($result);
-        if ($rows == 0) {
+        $rows = mysqli_num_rows($result); /*Dette gør, at hvis der er kontakt, så tjekker den antal af fundne rækker i databasen Users*/
+        if ($rows == 0) { /* hvis der ikke er nogen rækker som matcher, gives denne alert til brugeren */
             echo '<script>alert("Denne bruger eksisterer ikke")</script>';
         }
-        while($row = mysqli_fetch_assoc($result)){
+        while($row = mysqli_fetch_assoc($result)){ /*Her tester vi om alle tre linjer stemmer overens, samt omkoder vores hash password, tilbage til brugerens password - her gives de forskellige advarsler, samt at hvis alt er godt, vil brugeren blive sendt videre til minside.php */
             $mail = $row['mail'];
             $database_password = $row['password'];
             $user_id = $row['user_id'];
