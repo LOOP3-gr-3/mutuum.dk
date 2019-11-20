6576f7aba89e3ec90e62b97d5332d675341eab4e
Mette <metteweyergang@gmail.com>
2019-11-20 14:48:06 +0100

    kontrakt første knap

--- kontrakt_match.php
+++ kontrakt_match.php
@@ -1,12 +1,10 @@
-<!-- her henter jeg headeren ind og kontraktdatabasen -->
-<?php 
-    $page = ('Oprettelse af kontrakt');
+<!-- her henter jeg headeren ned -->
+<?php
+    $page = ('Mine aftaler');
     require_once('includes/header.php');
-require_once('includes/kontraktdatabase.php');
     if (isset($_SESSION['user_id'])) {
         header('location:forside.php');
 }
-     
 ?>
 
 <h1 id="xopretudlan">OPRET UDLÅN</h1>
@@ -18,138 +16,12 @@ require_once('includes/kontraktdatabase.php');
 <div class="container">
     <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#netveark" id="xnetveark">Låneftale til netværk</button>
 
-</div>
-
-    <!--Her vælger jeg indholdet der skal vises når der trykkes på den bestemte knap-->
-    <div id="netveark" class="collapse">
-</div>
-        <br>
-        <br>
-        <br>
-        <p id="beløb">Beløb</p>
-        
-        <div class="btn btn-light dropdown-toggle">
-            <select class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <?php  /* Dette henter værdierne fra vores tabel "beloeb" fra vores server ind til denne dropdown */  
-                if ($rowsbeloeb > 0) {
-                    echo "<option value='--'>Angiv beløb i DKK</option>";                
-                    while($rowbeloeb = mysqli_fetch_assoc($resultbeloeb)) {
-                    echo "<option value='" . $rowbeloeb['beloeb_id'] . "'>" . $rowbeloeb['beloeb'] . "</option>";
-                    } 
-                } 
-                mysqli_close($con);
-                ?>
-                
-                </div>
-                
-                </select>
-</div>
-            
-        <br>
-        <p id="beløb">Rente</p>
-
-        <div class="dropdown">
-            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                angiv rente i %
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">1%</a>
-                <a class="dropdown-item" href="#"> 2%</a>
-                <a class="dropdown-item" href="#"> 3%</a>
-            </div>
-        </div> <br>
-        <p id="beløb">Løbetid</p>
-
-        <div class="dropdown">
-            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                angiv perioden for lånet
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">1 måned</a>
-                <a class="dropdown-item" href="#"> 2 måneder</a>
-                <a class="dropdown-item" href="#">3 måneder</a>
-            </div>
-        </div>
-        <br>
-        <p>Månedligt afdrag</p>
-        <div class="dropdown">
-            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Udregnet mnd. afdrag
-            </button>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                <a class="dropdown-item" href="#">geneerated1</a>
-            </div>
-        </div> <br>
-        <p>Gebyr for oprettelse af kontrakt skal betales af:</p>
-
-        <!-- her laes valgmulighed 1, mig for betaling af gebyr-->
-        <div class="custom-control custom-checkbox custom-control-inline">
-            <input type="checkbox" class="custom-control-input" id="defaultInline1">
-            <label class="custom-control-label" for="defaultInline1">mig</label>
-        </div>
-
-        <!--Vælg betaler til at være låntager-->
-        <div class="custom-control custom-checkbox custom-control-inline">
-            <input type="checkbox" class="custom-control-input" id="defaultInline2">
-            <label class="custom-control-label" for="defaultInline2">Låntager</label>
-        </div> <br> <br>
-        <!--afsnit for kontraktbrud med menu for hvilke konsekvenser der skal være ved kontraktbrud-->
-        <strong>kontraktbrud</strong>
-        <div class="form-check">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" value="">Forhøj rente med 2%
-            </label>
-        </div>
-
-        <div class="form-check">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" value="">Vedlæg gebyr på:
-                <div class="dropdown">
-                    <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">vælg gebyr
-                    </button>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <a class="dropdown-item" href="#">100 , -</a>
-                        <a class="dropdown-item" href="#">200 , -</a>
-                        <a class="dropdown-item" href="#">300 , -</a>
-                    </div>
-                </div> <br>
-            </label>
-        </div>
-
-        <div class="form-check">
-            <label class="form-check-label">
-                <input type="checkbox" class="form-check-input" value="" disabled>Forlæng løbetid med 2 måneder
-            </label>
-        </div>
-        <!--Jeg er ikke helt sikker på hvordan I vil have boxen med den udregnet/generated value, men har bare lavet en U med value indeni, som symbolisere det for nu-->
-        <br><br>
-        <p><I>Udreget afkast = <U>Some generated value , -</U></I></p>
-
-        <!--Her kodes de to knapper i bunden, som skal føre til enten en siden hvor man underskriver eller sende kontrakten til mine aftaler under min profil, hvor den står som oprettede kontrakter-->
-        <!--Her skrive rjeg en tekst, den skal deles i to afsnist, deraf <br>, dernæst vælger jeg at skrive at kontrakten bliver gemt til mine aftaler, denne har jeg gjort til en knap med button, understreget med U og kursiv med I og til slut sluttes paragraffen med en </p>-->
-        <br><br>
-        <p><I>"Underskriv og gem", her sendes kontrakten ud, så en vilkårlig kan underskrive og indgå i en juridisk bindende kontrakt.
-                <br> "Gem låneaftale", så gemmes den under din profil --> <button type="button" href="mineaftaler.php" target="_blank"><U> mine aftaler</U></button></I></p>
-        <!--Her kodes de to knapper i bunden, som skal føre til enten en siden hvor man underskriver eller sende kontrakten til mine aftaler under min profil, hvor den står som oprettede kontrakter-->
-        <a href="underskrivoggem.php" target="_blank"><button type="button" class="btn btn-light">Underskriv og gem</button></a>
-
-        <!-- Den anden knap -->
-        <a href="kontrakt.php" target="_blank"><button type="button" class="btn btn-light">Gem låneaftale</button></a>
-        <br>
-        <br>
-        <!--Slut tag på container og på det der skulle collapses, altså vises når der bliver trykket på den knap der er angivet i containere, sluttagget af de to </div> -->
-
-    </div>
-</div>
-
 
 <!-- HER STARTER LÅNEAFTALE TIL VILKÅRLIG-->
 <div class="container">
-    <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#matchside" id="xmatchside" onclick="toggleMatch()">Låneftale til vilkårlig</button>
+    <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#matchside" id="xmatchside">Låneftale til vilkårlig</button>
 
     <div id="matchside" class="collapse">
-        <br>
         <br>
         <p id="beløb">Beløb</p>
 
@@ -271,7 +143,7 @@ require_once('includes/kontraktdatabase.php');
                 <br> "Gem låneaftale", så gemmes den under din profil --> <button type="button" href="mineaftaler.php" target="_blank"><U> mine aftaler</U></button></I></p>
         <!--Her kodes de to knapper i bunden, som skal føre til enten en siden hvor man underskriver eller sende kontrakten til mine aftaler under min profil, hvor den står som oprettede kontrakter-->
         <a href="underskrivoggem.php" target="_blank"><button type="button" class="btn btn-light">Underskriv og gem</button></a>
-
+        
         <br>
 
         <!-- Den anden knap -->
@@ -289,15 +161,3 @@ require_once('includes/kontraktdatabase.php');
 <!--<?php
     require_once('includes/footer.php');
 ?>-->
-<script>
-    function toggleNetvaerk(netvaerk, match) {
-        var netvaerk = 1;
-        var match = 0;
-    }
-
-    function toggleMatch(netvaerk, match) {
-        var match = 2;
-        var netvaerk = 0;
-    }
-
-</script>
