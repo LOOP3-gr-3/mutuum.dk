3e24572f7b91164a6eaea52c5883320c2264d29c
Lasse <lassehvisthule@outlook.dk>
2019-11-21 14:51:14 +0100

    Update

--- minside.php
+++ minside.php
@@ -4,13 +4,13 @@ require_once('includes/header.php');
 
 
 
-if(!isset($_SESSION['user_id'])) {
+/*if(!isset($_SESSION['user_id'])) {
         echo '<script>alert("Du er ikke logget ind på MUTUUM - log ind her, eller opret en bruger og få gratis adgang til platformen!");';
         echo 'window.location.href="login.php";';
         echo '</script>' ;
         die();   
-     /*Her bliver brugeren dirigeret til login, hvis de ikke er logget ind */
-}                       
+     /*Her bliver brugeren dirigeret til login, hvis de ikke er logget ind */ 
+                       
 ?>
 
 
@@ -22,7 +22,7 @@ if(!isset($_SESSION['user_id'])) {
 
 <div id="site'highlights" style="text-align:center">
     <hr>
-        <h1><strong>Min Side</strong></h1>
+        <h1><strong>Min side</strong></h1>
     <hr>
 </div>
 
@@ -30,65 +30,46 @@ if(!isset($_SESSION['user_id'])) {
 <div id="container-las">
     <div id="container1-5">
         <div id="l-billede">
-            <a href="minprofil.php"><img id="billede1" src="images/profil.png"></a>
+            <a href="minprofil.php"><img id="billede1" src="images/profilny.png"></a>
         </div>
         
-        <div id="l-tekst">
-            <a href="minprofil.php">Min profil</a>
-            
-        </div>
+        
 
     </div>
     
     <div id="container2-5">
         <div id="l-billede">
-            <a href="KFUM.php"><img id="billede1" src="images/netvaerk.png"></a>
+            <a href="KFUM.php"><img id="billede1" src="images/r%C3%A5dgivningny-kopi.png"></a>
             
         </div>
         
-        <div id="l-tekst">
-            <a href="KFUM.php">Rådgivning</a>
-            
-        </div>
+        
     </div>
     
     
     <div id="container3-5">
         <div id="l-billede">
-            <a href="matchsite.php"><img id="billede1" src="images/match.png"></a>
+            <a href="matchsite.php"><img id="billede1" src="images/matchny-kopi.png"></a>
         </div>
         
-        <div id="l-tekst">
-            <a href="matchsite.php">Match site</a>
-            
-        </div>
+        
     </div>
     
     
     <div id="container4-5">
         <div id="l-billede">
-            <a href="kontrakt.php" taget="_blank"><img id="billede1" src="images/kontrakt.png"></a>
+            <a href="kontrakt.php" taget="_blank"><img id="billede1" src="images/kontraktny.png"></a>
         </div>
         
-        <div id="l-tekst">
-
-            <a href="OPRETKONTRAKT">Kontrakt</a>
-
-            <a href="kontrakt.php">Opret kontrakt</a>
-
-            
-        </div>
+        
     </div>
     
     <div id="container5-5">
         <div id="l-billede">
-            <a href="statistik.php"><img id="billede1" src="images/hej2.png"></a>
+            <a href="statistik.php"><img id="billede2" src="images/statistikny.png"></a>
         </div>
         
-        <div id="l-tekst">
-            <a href="statistik.php">Statistik</a>
-            
-        </div>
+        
     </div>
 </div>
 </div>
